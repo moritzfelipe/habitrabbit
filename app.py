088bb62be7ad5d52de3dd3c9408f8910c7f09762
moritzfelipe <stellmacher@gmail.com>
2017-04-12 15:04:20 +0000

    Added PostgreSQL environment variable.

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ from resources.habit import CreateHabit, DeleteHabit, UpdateHabit
 from resources.user import User, UserList, GetUser
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ['HRABBIT_SECRET']
 api = Api(app)
-
 
 api.add_resource(User, '/user/')
 api.add_resource(GetUser, '/getuser/<string:msg_id>')
