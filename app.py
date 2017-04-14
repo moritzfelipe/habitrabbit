fa3dd80345b51f79b757cba4674f17909b573e68
moritzfelipe <stellmacher@gmail.com>
2017-04-14 13:31:53 +0000

    test if problem with app key 2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.secret_key = os.environ['HRABBIT_SECRET']
-app.secret_key = "secrettest"
+app.secret_key = 'secrettestkey'
 api = Api(app)
 
 api.add_resource(User, '/user/')
